b93a8e30eeff7cdbecd8730c5788e87054729a19
guistetter <guilherme_stetter@hotmail.com>
2020-09-01 09:57:33 -0300

    primeira rota ok

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res)=>{
- res.send('Full Stack')
+  const items = [{name: 'teste1'}, {name: 'teste2'}]
+ res.render('index',{title: 'FullStack-Projeto-Pratico', items})
 })
 
 app.listen(3000, () => "server is running...")
